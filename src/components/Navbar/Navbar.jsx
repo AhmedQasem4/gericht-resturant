9562acc355969c380a58b3687f8d49c62072c158
AhmedQasem <abok2ssem14@gmail.com>
2023-03-15 16:08:53 +0200

    build navbar until mobile version.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
-
 import './Navbar.css';
+import images from '../../constants/images';
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <nav className="app__navbar">
+    <div className="app__navbar-logo">
+      <img src={images.gericht} alt="resturant logo" />
+    </div>
+    <div className="app__navbar-links">
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Menu</a></li>
+        <li><a href="#">Awards</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </div>
+    <div className="app__navbar-login">
+      <a href="">Login / Registration</a>
+      <div/>
+      <a href="#">book table</a>
+    </div>
+  </nav>
 );
 
 export default Navbar;
