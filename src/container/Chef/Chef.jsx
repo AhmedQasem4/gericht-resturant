d27f57354aab1dad51a46393bb05c60585d27680
AhmedQasem <abok2ssem14@gmail.com>
2023-03-21 13:35:37 +0200

    build chef section and we skip intro section.

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
-
-import './Chef.css';
+import React from "react";
+import images from "../../constants/images";
+import "./Chef.css";
+import SubHeading from "./../../components/SubHeading/SubHeading";
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__chef app__bg section__padding">
+    <div className="app__chef-image">
+      <img src={images.chef} alt="chef image" />
+    </div>
+    <div className="app__chef-content">
+      <SubHeading title="Chef's words" />
+      <h1 style={{marginBottom: "2rem",}} className="headtext__cormorant">What we belive in</h1>
+      <div className="app__chef-content_quote">
+        <img src={images.quote} alt="quote logo" />
+        <p className="p__opensans">
+          Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit Auctor Sit .
+        </p>
+      </div>
+        <p className="app__chef-content_p">
+          Auctor Sit Iaculis In Arcu. Vulputate Nulla Lobortis Mauris Eget Sit.
+          Nulla Scelerisque Scelerisque Congue Ac Consequat, Aliquam Molestie
+          Lectus Eu. Congue Iaculis Integer Curabitur Semper Sit Nunc.
+        </p>
+      <div className="app__chef-content_sign">
+        <p className="chef_name">Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="chef signature" />
+      </div>
+    </div>
   </div>
 );
 
