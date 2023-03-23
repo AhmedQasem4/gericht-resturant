1275666c88444e6067abda92e7a7883e86bab467
ahmed Qasem <abok2ssem14@gmail.com>
2023-03-22 17:59:06 -0700

    building footer.

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import SubHeading from '../SubHeading/SubHeading';
 
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className='app__footer-newsletter'>
+    <SubHeading title="Newsletter" />
+    <h1 style={{marginTop: "2rem"}} className="headtext__cormorant">Subscribe to our news letter</h1>
+    <p className="p__opensans">And Never Miss latest Updates</p>
+    <div className="app__footer-newsletter_input">
+    <input type="text" placeholder='Enter your email address' />
+    <button className="custom__button">Subscribe</button>
+    </div>
   </div>
 );
 
